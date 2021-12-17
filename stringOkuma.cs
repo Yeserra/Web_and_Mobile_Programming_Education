60d461fa32a4b23fd71215b5e049f398df56cd1b
Yeserra <96071629+Yeserra@users.noreply.github.com>
2021-12-17 18:08:20 +0300

    Update stringOkuma.cs

--- stringOkuma.cs
+++ stringOkuma.cs
@@ -21,7 +21,7 @@ namespace Alistirma
             {
                 for (i = 1; i < str.Length; i++)
                 {
-                    if (str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9' || str[i] == '.')
+                    if (str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9')
                     {
                         continue;
                     }
@@ -43,11 +43,11 @@ namespace Alistirma
                     Console.WriteLine("Girilen kelime ondalıklı bir sayı değildir!");
                 }
             }
-            else if (str[0] == '-' || str[0] == '+' || str[0] == '1' || str[0] == '2' || str[0] == '3' || str[0] == '4' || str[0] == '5' || str[0] == '6' || str[0] == '7' || str[0] == '8' || str[0] == '9')
+            else if (str[0] == '-' || str[0] == '+' || str[0] == '1' || str[0] == '2' || str[0] == '3' || str[0] == '4' || str[0] == '5' || str[0] == '6' || str[0] == '7' || str[0] == '8' || str[0] == '9' && str.Contains('.'))
             {
                 for (i = 1; i < str.Length; i++)
                 {
-                    if (str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9' || str[i] == '.')
+                    if (str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9')
                     {
                         continue;
                     }
