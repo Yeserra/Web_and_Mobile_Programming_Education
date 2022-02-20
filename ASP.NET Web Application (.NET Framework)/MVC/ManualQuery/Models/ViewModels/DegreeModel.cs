551954524519873986eca840638f23f55cd1577c
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-02-20 19:16:44 +0300

    Create DegreeModel.cs

---
+++ ASP.NET Web Application (.NET Framework)/MVC/ManualQuery/Models/ViewModels/DegreeModel.cs
@@ -0,0 +1,16 @@
+using ManualQuery.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManualQuery.Models.ViewModels
+{
+    public class DegreeModel
+    {
+        public Degree Degree { get; set; }
+        public string BtnVal { get; set; }
+        public string BtnClass { get; set; }
+        public string Title { get; set; }
+    }
+}
