f5660f320617950d9a7dce919ce08f72675b1c04
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-02-20 19:15:26 +0300

    Create Degree.cs

---
+++ ASP.NET Web Application (.NET Framework)/MVC/ManualQuery/Models/Classes/Degree.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManualQuery.Models.Classes
+{
+    public class Degree
+    {
+        public int DegreeId { get; set; }
+        public string DegreeName { get; set; }
+    }
+}
