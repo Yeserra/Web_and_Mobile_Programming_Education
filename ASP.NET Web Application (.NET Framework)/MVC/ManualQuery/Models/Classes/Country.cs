8b6ae89e23e3c9d4eae2a720884f1841befd8405
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-02-20 19:15:45 +0300

    Create Country.cs

---
+++ ASP.NET Web Application (.NET Framework)/MVC/ManualQuery/Models/Classes/Country.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManualQuery.Models.Classes
+{
+    public class Country
+    {
+        public string CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+}
