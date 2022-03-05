911f0b0b1f00f4e90c1939faf1ccc05cfdf3006d
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-03-06 00:58:55 +0300

    Create DegreeDTO.cs

---
+++ ASP.NET Web Application (.NET Framework)/Empty/DataModel_EF/Models/DTOs/DegreeDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataModel_EF.Models.DTOs
+{
+    public class DegreeDTO
+    {
+        public int DegreeId { get; set; }
+        public string DegreeName { get; set; }
+    }
+}
