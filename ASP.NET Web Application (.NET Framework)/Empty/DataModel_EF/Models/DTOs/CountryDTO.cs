5b77577df79e666c9f6f6c5bf456be4ad5a83f06
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-03-06 00:59:21 +0300

    Create CountryDTO.cs

---
+++ ASP.NET Web Application (.NET Framework)/Empty/DataModel_EF/Models/DTOs/CountryDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataModel_EF.Models.DTOs
+{
+    public class CountryDTO
+    {
+        public string CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+}
