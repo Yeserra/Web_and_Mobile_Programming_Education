86ce4e43ed4d6ff0ae6230623dc67a1e6b1f1e9e
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-03-06 00:58:17 +0300

    Create ManagerDTO.cs

---
+++ ASP.NET Web Application (.NET Framework)/Empty/DataModel_EF/Models/DTOs/ManagerDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataModel_EF.Models.DTOs
+{
+    public class ManagerDTO
+    {
+        public int ManagerId { get; set; }
+        public string FullName { get; set; }
+    }
+}
