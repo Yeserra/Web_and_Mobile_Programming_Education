8fa6156f8eaf1eb36b3c76d02234232ece042384
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-03-06 18:23:50 +0300

    Update StudentDTO.cs

--- ASP.NET Web Application (.NET Framework)/MVC/CodeFirst_EF/Models/DTOs/StudentDTO.cs
+++ ASP.NET Web Application (.NET Framework)/MVC/CodeFirst_EF/Models/DTOs/StudentDTO.cs
@@ -5,9 +5,12 @@ using System.Web;
 
 namespace CodeFirst_EF.Models.DTOs
 {
-    public class CityDTO
+    public class StudentDTO
     {
-        public int CityId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
         public string CityName { get; set; }
+        public string MotherName { get; set; }
     }
 }
