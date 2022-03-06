358e21077b8bebc7ebf0158cd04db472df6ca146
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-03-06 18:23:13 +0300

    Create StudentDTO.cs

---
+++ ASP.NET Web Application (.NET Framework)/MVC/CodeFirst_EF/Models/DTOs/StudentDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst_EF.Models.DTOs
+{
+    public class CityDTO
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+    }
+}
