145cc0a9b9b74b9d0fbf7a394848ada16d801b71
Yeserra <96071629+Yeserra@users.noreply.github.com>
2022-03-07 00:12:39 +0300

    Create EmployeeDTO.cs

---
+++ ASP.NET Core Web  App (Model-View-Controller)/SimpleCore/Models/DTOs/EmployeeDTO.cs
@@ -0,0 +1,10 @@
+namespace SimpleCore.Models.DTOs
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CityName { get; set; }
+    }
+}
